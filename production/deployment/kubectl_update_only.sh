557e341be77bbb06be1fe12975525cae17c75a83
Mohammad Wasil <mwasil@outlook.co.id>
2018-09-26 22:32:37 +0200

    rename namespace and release to prod (production)

--- production/deployment/kubectl_update_only.sh
+++ production/deployment/kubectl_update_only.sh
@@ -1,4 +1,4 @@
 helm repo update
-RELEASE=e2-jhub
-NAMESPACE=e2-jhub
-helm upgrade $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version 0.7.0 --values config/config_update.yaml
+RELEASE=prod
+NAMESPACE=prod
+helm upgrade $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version 0.7.0 --values config/config.yaml
