30058ebe47019f4e822818172777a69c7362ea59
Mohammad Wasil <mwasil@outlook.co.id>
2018-11-04 17:36:11 +0100

    modify kubectl_jhub_install

--- development/deployment/kubectl_jhub_install.sh
+++ development/deployment/kubectl_jhub_install.sh
@@ -3,4 +3,4 @@ helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 helm repo update
 RELEASE=dev-datahub
 NAMESPACE=dev-datahub
-helm upgrade --install $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version 0.7.0 --values config/config.yaml
+#helm upgrade --install $RELEASE jupyterhub/jupyterhub --namespace $NAMESPACE --version 0.7.0 --values config/config.yaml
